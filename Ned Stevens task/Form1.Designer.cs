23d07af631b7775775e172d60e1acd2dcb180db4
Gavin Alan <Gavin Alan>
2017-04-19 23:49:12 -0400

    Add project files.

---
+++ Ned Stevens task/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Ned_Stevens_task
+{
+    partial class frmContact
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtfullName = new System.Windows.Forms.TextBox();
+            this.txtfullAddress = new System.Windows.Forms.TextBox();
+            this.txtphoneNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgContact = new System.Windows.Forms.DataGridView();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgContact)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(360, 78);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(152, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add Contact";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtfullName
+            // 
+            this.txtfullName.Location = new System.Drawing.Point(102, 26);
+            this.txtfullName.Name = "txtfullName";
+            this.txtfullName.Size = new System.Drawing.Size(410, 20);
+            this.txtfullName.TabIndex = 1;
+            // 
+            // txtfullAddress
+            // 
+            this.txtfullAddress.Location = new System.Drawing.Point(102, 52);
+            this.txtfullAddress.Name = "txtfullAddress";
+            this.txtfullAddress.Size = new System.Drawing.Size(410, 20);
+            this.txtfullAddress.TabIndex = 2;
+            // 
+            // txtphoneNumber
+            // 
+            this.txtphoneNumber.Location = new System.Drawing.Point(102, 78);
+            this.txtphoneNumber.Name = "txtphoneNumber";
+            this.txtphoneNumber.Size = new System.Drawing.Size(252, 20);
+            this.txtphoneNumber.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Full Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Phone #";
+            // 
+            // dgContact
+            // 
+            this.dgContact.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgContact.Location = new System.Drawing.Point(45, 165);
+            this.dgContact.Name = "dgContact";
+            this.dgContact.Size = new System.Drawing.Size(467, 269);
+            this.dgContact.TabIndex = 7;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(360, 107);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(152, 23);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Search by phone #";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // frmContact
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 597);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.dgContact);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtphoneNumber);
+            this.Controls.Add(this.txtfullAddress);
+            this.Controls.Add(this.txtfullName);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "frmContact";
+            this.Text = "Contact information";
+            ((System.ComponentModel.ISupportInitialize)(this.dgContact)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtfullName;
+        private System.Windows.Forms.TextBox txtfullAddress;
+        private System.Windows.Forms.TextBox txtphoneNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgContact;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ Ned Stevens task/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Ned_Stevens_task
+{
+    public partial class frmContact : Form
+    {
+        DataSet dataSet = new DataSet();
+        SqlConnection connectionString = new SqlConnection("Data Source=ALAN-FAMILY\\TESTLAB;Initial Catalog=testLab;Integrated Security=True");
+        SqlDataAdapter dataAdapter = new SqlDataAdapter();
+
+
+        public frmContact()
+        {
+            InitializeComponent();
+        }
+
+        void ClearAllText(Control con)
+        {
+            foreach (Control c in con.Controls)
+            {
+                if (c is TextBox)
+                    ((TextBox)c).Clear();
+                else
+                    ClearAllText(c);
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataAdapter.InsertCommand = new SqlCommand("INSERT INTO tblContacts VALUES(@fullName, @fullAddress, @phoneNumber)", connectionString);
+            dataAdapter.InsertCommand.Parameters.Add("@fullName", SqlDbType.VarChar).Value = txtfullName.Text;
+            dataAdapter.InsertCommand.Parameters.Add("@fullAddress", SqlDbType.VarChar).Value = txtfullAddress.Text;
+            dataAdapter.InsertCommand.Parameters.Add("@phoneNumber", SqlDbType.VarChar).Value = txtphoneNumber.Text;
+
+            connectionString.Open();
+            dataAdapter.InsertCommand.ExecuteNonQuery();
+            connectionString.Close();
+
+            dataAdapter.SelectCommand = new SqlCommand("SELECT * FROM tblContacts ", connectionString);
+            dataSet.Clear();
+            dataAdapter.Fill(dataSet);
+            dgContact.DataSource = dataSet.Tables[0];
+
+            ClearAllText(this);
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            dataAdapter.SelectCommand = new SqlCommand("SELECT * FROM tblContacts WHERE phoneNumber = @phoneNumber  ", connectionString);
+            dataAdapter.SelectCommand.Parameters.Add("@phoneNumber", SqlDbType.VarChar).Value = txtphoneNumber.Text;
+            dataSet.Clear();
+            dataAdapter.Fill(dataSet);
+            dgContact.DataSource = dataSet.Tables[0];
+            ClearAllText(this);
+
+        }
+    }
+}
